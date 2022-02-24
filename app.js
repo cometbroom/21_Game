88d0652d611d00849d5a07eb1bd8acff3c67ce47
cometbroom <alawi.wow@gmail.com>
2022-02-24 17:31:01 +0100

    Added turn indicator

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 const clog = (target) => {
     console.log(target);
 };
@@ -6,15 +10,32 @@ const getNumber = (el) => {
     return parseInt(el.innerHTML, 10);
 };
 
-const addButtonEvents = (parent, winCallback) => {
+const addToTotal = (x) => {
+    const newTotal = x + getNumber(totalDisplay);
+    totalDisplay.innerHTML = newTotal;
+    return newTotal;
+};
+
+const changeTurnStyle = (el, actor) => {
+    if (actor === "computer") {
+        el.children[0].style.opacity = "0.2";
+        el.children[1].style.opacity = "1";
+    } else {
+        el.children[1].style.opacity = "0.2";
+        el.children[0].style.opacity = "1";
+    }
+};
+
+const addButtonEvents = (parent, winCallback, computerTurn) => {
     for (const element of parent.children) {
         element.addEventListener("click", (e) => {
             let targetNum = parseInt(e.target.innerHTML, 10);
-            const newTotal = targetNum + getNumber(totalDisplay);
-            totalDisplay.innerHTML = newTotal;
-            if (newTotal === 21) {
+            const total = addToTotal(targetNum);
+            if (total === 21) {
                 winCallback();
+                return;
             }
+            computerTurn();
         });
     }
 };
@@ -26,12 +47,29 @@ document.addEventListener("DOMContentLoaded", () => {
     const button2 = document.querySelector("#btn2");
     const button3 = document.querySelector("#btn3");
     const extraInfo = document.querySelector(".extra-info");
+    const turnDisplay = document.querySelector(".turn-display");
 
     const winCondition = () => {
         extraInfo.innerHTML = "You win!";
     };
 
-    addButtonEvents(buttons, winCondition);
+    const computerTurn = async () => {
+        const options = [1, 2, 3];
+        const randIndex = Math.floor(Math.random() * options.length);
+        let total;
 
-    clog(buttons.children);
+        //turnDisplay.children[0].style;
+        changeTurnStyle(turnDisplay, "computer");
+        await sleep(2000).then(() => {
+            total = addToTotal(options[randIndex]);
+        });
+        if (total === 21) {
+            extraInfo.innerHTML = "Computer won!";
+        }
+        changeTurnStyle(turnDisplay, "player");
+    };
+
+    addButtonEvents(buttons, winCondition, computerTurn);
+
+    //clog(buttons.children);
 });
