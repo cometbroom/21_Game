0b747d1eb2d16c95aeeb16ed5c2b4af389f75656
cometbroom <alawi.wow@gmail.com>
2022-02-24 16:57:48 +0100

    Created event listeners

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ const clog = (target) => {
     console.log(target);
 };
 
+const getNumber = (el) => {
+    return parseInt(el.innerHTML, 10);
+};
+
+const addButtonEvents = (parent, winCallback) => {
+    for (const element of parent.children) {
+        element.addEventListener("click", (e) => {
+            let targetNum = parseInt(e.target.innerHTML, 10);
+            const newTotal = targetNum + getNumber(totalDisplay);
+            totalDisplay.innerHTML = newTotal;
+            if (newTotal === 21) {
+                winCallback();
+            }
+        });
+    }
+};
+
 document.addEventListener("DOMContentLoaded", () => {
     const totalDisplay = document.querySelector("#totalDisplay");
     const buttons = document.querySelector(".buttons");
@@ -10,23 +27,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const button3 = document.querySelector("#btn3");
     const extraInfo = document.querySelector(".extra-info");
 
-    const getNumber = (el) => {
-        return parseInt(el.innerHTML, 10);
-    };
     const winCondition = () => {
         extraInfo.innerHTML = "You win!";
     };
 
-    clog(buttons.children);
+    addButtonEvents(buttons, winCondition);
 
-    for (const element of buttons.children) {
-        element.addEventListener("click", (e) => {
-            let targetNum = parseInt(e.target.innerHTML, 10);
-            const newTotal = targetNum + getNumber(totalDisplay);
-            totalDisplay.innerHTML = newTotal;
-            if (newTotal === 21) {
-                winCondition();
-            }
-        });
-    }
+    clog(buttons.children);
 });
