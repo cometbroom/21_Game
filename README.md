eb10670c827778210dc29475a7384180fe03b76a
cometbroom <alawi.wow@gmail.com>
2022-05-02 15:17:31 +0200

    Cleaned up the project and added comments

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # 21_Game
+
+A simple web game of 21. This is nothing like blackjack by the way. Sorry to disappoint.
+## Rules
+
+21 Is a game where you can choose numbers 1-3 against a computer or another player. Goal of the game is for the winner to reach 21 first:
+
+![Screenshot of the game page](./github-assets/Game%20page.PNG)
+
+## Development
+
+I made this game with HTML, SASS and vanilla javascript. Everything was made in the app.js file as the app wasn't supposed to get too large. I'm still hoping to turn this into a modular architecture in the future.
+
+This was also my first trivial experience with AI, it was a very fun way to learn about AI with much of the complexity lifted.
+
+### Hope you enjoy this!
--- app.js
+++ app.js
@@ -1,130 +1,109 @@
-//3 ai difficulties: harder means deeper prediction pattern: 17 medium, 13 hard.
-// easy is the current which has predictionPattern towards 18 (computer turn)
-
 //Globals
 let lockPlayer = false;
 const sounds = {
-    uiSound1: new Audio("./assets/sounds/Game_Invite.wav"),
-    playerWon: new Audio("./assets/sounds/Negative_Bell_Bling_Game_Sound.wav"),
-    computerWon: new Audio("./assets/sounds/Robot_Whoosh_Notification.wav"),
-};
-
-function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-const clog = (target) => {
-    console.log(target);
-};
-
-const getNumber = (el) => {
-    return parseInt(el.innerHTML, 10);
-};
-
-const addToTotal = (x) => {
-    const newTotal = x + getNumber(totalDisplay);
-    totalDisplay.innerHTML = newTotal;
-    return newTotal;
-};
-
-const changeTurnStyle = (el, actor) => {
-    const duration = 0.3;
-
-    sounds.uiSound1.play();
-
-    if (actor === "computer") {
-        gsap.to(el.children[0], { opacity: 0.2, duration });
-        gsap.to(el.children[1], { opacity: 1, duration });
-
-        //el.children[0].style.opacity = "0.2";
-        //el.children[1].style.opacity = "1";
-    } else {
-        gsap.to(el.children[0], { opacity: 1, duration });
-        gsap.to(el.children[1], { opacity: 0.2, duration });
-    }
+  uiSound1: new Audio("./assets/sounds/Game_Invite.wav"),
+  playerWon: new Audio("./assets/sounds/Negative_Bell_Bling_Game_Sound.wav"),
+  computerWon: new Audio("./assets/sounds/Robot_Whoosh_Notification.wav"),
 };
 
 let winCondition;
 let computerTurn;
 
+//Wait for document to load.
+document.addEventListener("DOMContentLoaded", () => {
+  const totalDisplay = document.querySelector("#totalDisplay");
+  const buttons = document.querySelector(".buttons");
+  const extraInfo = document.querySelector(".extra-info");
+  const turnDisplay = document.querySelector(".turn-display");
+
+  //Display winner when they win and turn off events.
+  winCondition = (winner) => {
+    extraInfo.textContent = `${winner} wins!`;
+    sounds[`${winner.toLowerCase()}Won`].play();
+
+    deleteBtnEvents(buttons);
+  };
+
+  //Method for AI to predict win and choose a number accordingly
+  const checkPossibleWin = (options) => {
+    for (const number of options) {
+      if (getNumber(totalDisplay) + number === 21) return number;
+    }
+    return null;
+  };
+
+  computerTurn = async () => {
+    //Look at the options
+    const options = [1, 2, 3];
+    const randIndex = Math.floor(Math.random() * options.length);
+    const randSleep = Math.floor(Math.random() * (1500 - 500 + 1) + 500);
+
+    //Lockplayer while computer is taking a turn.
+    lockPlayer = true;
+
+    //CSS to change style of the player taking turn
+    changeTurnStyle(turnDisplay, "computer");
+    await sleep(randSleep);
+    let nextMove = checkPossibleWin(options) || options[randIndex];
+    const total = addToTotal(nextMove);
+    if (total === 21) return winCondition("Computer");
+    if (total > 21) return winCondition("No one");
+
+    changeTurnStyle(turnDisplay, "player");
+    lockPlayer = false;
+  };
+
+  addButtonEvents(buttons, winCondition, computerTurn);
+});
+
 function btnClicked(e) {
-    if (lockPlayer === true) return;
-    let targetNum = parseInt(e.target.innerHTML, 10);
-    const total = addToTotal(targetNum);
-    if (total === 21) {
-        winCondition("Player");
-        return;
-    }
-    if (total > 21) {
-        winCondition("No one");
-        return;
-    }
-    computerTurn();
+  if (lockPlayer === true) return;
+  let targetNum = parseInt(e.target.innerHTML, 10);
+  const total = addToTotal(targetNum);
+  if (total === 21) return winCondition("Player");
+  if (total > 21) return winCondition("No one");
+  computerTurn();
 }
 
+const changeTurnStyle = (el, actor) => {
+  const duration = 0.3;
+
+  sounds.uiSound1.play();
+
+  if (actor === "computer") {
+    //gsap is animation library added in index.html
+    gsap.to(el.children[0], { opacity: 0.2, duration });
+    gsap.to(el.children[1], { opacity: 1, duration });
+  } else {
+    gsap.to(el.children[0], { opacity: 1, duration });
+    gsap.to(el.children[1], { opacity: 0.2, duration });
+  }
+};
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+//Get number inside element
+const getNumber = (el) => {
+  return parseInt(el.innerHTML, 10);
+};
+
+//Add number x to total result
+const addToTotal = (x) => {
+  const newTotal = x + getNumber(totalDisplay);
+  totalDisplay.innerHTML = newTotal;
+  return newTotal;
+};
+
 const addButtonEvents = (parent) => {
-    for (const element of parent.children) {
-        element.addEventListener("click", btnClicked);
-    }
+  for (const element of parent.children) {
+    element.addEventListener("click", btnClicked);
+  }
 };
 
 const deleteBtnEvents = (parent) => {
-    for (const element of parent.children) {
-        element.removeEventListener("click", btnClicked);
-    }
+  for (const element of parent.children) {
+    element.removeEventListener("click", btnClicked);
+  }
 };
-
-document.addEventListener("DOMContentLoaded", () => {
-    const totalDisplay = document.querySelector("#totalDisplay");
-    const buttons = document.querySelector(".buttons");
-    const button1 = document.querySelector("#btn1");
-    const button2 = document.querySelector("#btn2");
-    const button3 = document.querySelector("#btn3");
-    const extraInfo = document.querySelector(".extra-info");
-    const turnDisplay = document.querySelector(".turn-display");
-
-    winCondition = (winner) => {
-        extraInfo.textContent = `${winner} wins!`;
-        sounds[`${winner.toLowerCase()}Won`].play();
-
-        deleteBtnEvents(buttons);
-    };
-
-    const checkPossibleWin = (options) => {
-        for (const number of options) {
-            if (getNumber(totalDisplay) + number === 21) return number;
-        }
-        return -1;
-    };
-
-    computerTurn = async () => {
-        const options = [1, 2, 3];
-        const randIndex = Math.floor(Math.random() * options.length);
-        const randSleep = Math.floor(Math.random() * (1500 - 500 + 1) + 500);
-        let total;
-
-        lockPlayer = true;
-
-        //turnDisplay.children[0].style;
-        changeTurnStyle(turnDisplay, "computer");
-        await sleep(randSleep).then(() => {
-            let possibleWinNum = checkPossibleWin(options);
-            if (possibleWinNum === -1) total = addToTotal(options[randIndex]);
-            else total = addToTotal(possibleWinNum);
-        });
-        if (total === 21) {
-            winCondition("Computer");
-            return;
-        }
-        if (total > 21) {
-            winCondition("No one");
-            return;
-        }
-        changeTurnStyle(turnDisplay, "player");
-        lockPlayer = false;
-    };
-
-    addButtonEvents(buttons, winCondition, computerTurn);
-
-    //clog(buttons.children);
-});
